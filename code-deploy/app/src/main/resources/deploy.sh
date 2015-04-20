0bd1d49c0273b20f27a69a96d39fe6a71d8462a1
Anurag Kapur <anurag@beancrunch.com>
2015-04-20 21:49:09 +0100

    Working version of a flaky deploy script

--- code-deploy/app/src/main/resources/deploy.sh
+++ code-deploy/app/src/main/resources/deploy.sh
@@ -1,1 +1,49 @@
 #!/usr/bin/env bash
+
+## TODO: Add checks to prevent concurrent execution of this script
+
+SOURCE_TAR_URL=$1
+WORKSPACE=/opt/code-deploy/workspace
+
+downloadAndUntar ()
+{
+    sourceTarUrl=$1
+    slugName=code-deploy-slug.tar
+
+    rm -rf $WORKSPACE
+    mkdir -p $WORKSPACE
+    cd $WORKSPACE
+    curl -o $slugName $sourceTarUrl
+    tar -xvf $slugName
+}
+
+processProcFile ()
+{
+    cd $WORKSPACE
+    applicationType=`cat Procfile | awk -F':' '{print $2}' | awk -F' ' '{print $1}'`
+    runCommand=`cat Procfile | awk -F':' '{print $2}'`
+
+    # Setup pre-requisistes
+    if [ "$applicationType" == "java" ]
+    then
+        echo "java..."
+        sudo yum -y install java
+    elif [ "$applicationType" = "node" ]
+    then
+        echo "node..."
+        # TODO: install node
+    fi
+
+    # Run binary
+    echo "will run command ${runCommand}..."
+    $runCommand > /tmp/code-deploy.log &
+}
+
+main ()
+{
+    sourceTarUrl=$1
+    downloadAndUntar $sourceTarUrl
+    processProcFile
+}
+
+main $SOURCE_TAR_URL
