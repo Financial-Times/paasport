551c1447ab891b8a1cc8c1f0e4cf03f37c7d6258
Willem Koopman <willem@portal.config>
2015-04-21 11:26:16 +0100

    Added in a user_data function

--- provisioner/models/machine.py
+++ provisioner/models/machine.py
@@ -1,8 +1,31 @@
+import os
 import boto.ec2
 
 # EU-WEST-1: RHEL7 HVM
 AMI_ID = 'ami-25158352'
 region = 'eu-west-1'
+
+def generate_userdata():
+	'''Read environment vars and various other things to
+	get SSH keys, usernames and deploy scripts and inject them into an
+	instance as a userdata script
+	'''
+	ssh_key					= os.environ["DEPLOY_SSH_KEY"]
+	deploy_script_location	= os.environ["DEPLOY_SCRIPT_LOCATION"] 
+	with open(deploy_script_location, 'r') as deploy_handle:
+		deploy_script = deploy_handle.readlines()
+	deploy_handle.close()
+	user_data = '''#!/usr/bin/bash
+
+	printf '{0}' >> /home/ec2-user/.ssh/authorized_keys;
+	echo {1} > /opt/code-deploy/deploy.sh
+	sudo chmod a+x /opt/code-deploy/deploy.sh
+	echo "all done"
+
+	'''.format(ssh_key, deploy_script)
+	print user_data
+
+	return base64.b64encode(user_data) 
 
 def create_many(definitions, cluster_id):
 	return map(format_instance, map(lambda data: create_new(data, cluster_id),
@@ -23,7 +46,7 @@ def create_new(data, cluster_id):
 def create_new_in_nursery(data):
 	security_groups = [ 'sg-fb4c1a9e' ]
 	connection = boto.ec2.connect_to_region(region)
-	instance = connection.run_instances(AMI_ID, instance_type='m3.medium').instances[0]
+	instance = connection.run_instances(AMI_ID, instance_type='m3.medium', user_data=generate_userdata()).instances[0]
 	connection.create_tags([instance.id], { 'cluster': '__nursery__' })
 
 	return instance
