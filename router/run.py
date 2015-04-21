050925d291bd723fd96c0a5a5cabf1db4f7a0861
Luke Blaney <luke.blaney@ft.com>
2015-04-21 10:33:01 +0100

    Support addons in router

--- router/run.py
+++ router/run.py
@@ -15,29 +15,117 @@ class UserError(Exception):
 
 
 
-def writedefaultvcl ():
+def writedefaultvcl():
 	vcl = "vcl 4.0;\n\nimport directors;\n"
 	services = []
-	for filename in os.listdir('/etc/varnish/services'):
-		vcl += "include \"services/"+filename+"\";\n"
+
+	for filename in os.listdir('/etc/varnish/addon'):
+		vcl += "include \"addon/"+filename+"\";\n"
+
+	for filename in os.listdir('/etc/varnish/service'):
+		vcl += "include \"service/"+filename+"\";\n"
 		services.append(filename.split('.',1)[0])
 
-	vcl += "\n\nbackend default {\n\t.host = \"127.0.0.1\";\n\t.port = \"8080\";\n}\n\nsub vcl_recv {\n"
+
+	vcl += "\n\nbackend default {\n\t.host = \"127.0.0.1\";\n\t.port = \""+PORT_NUMBER+"\";\n}\n\nsub vcl_recv {\n"
 
 	for service in services:
 		vcl += "\tcall service_"+service+";\n"
 	vcl += "}\n\n"
 
 
-	subprocess.check_call(["sudo", "service", "varnish", "reload"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-
-
 	# Write the VCL to disk
 	with open('/etc/varnish/default.vcl', 'w') as outfile:
 		outfile.write(vcl)
 
+	subprocess.check_call(["sudo", "service", "varnish", "reload"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+# Writes config for a given service
+def writeservice(servicetype, data):
+
+	# Validate inputs
+	machines = data.setdefault('machines', [])
+	addons = data.setdefault('addons', [])
+	if not isinstance(machines, list):
+		raise UserError('The property "machines" must be a list')
+	if not isinstance(addons, list):
+		raise UserError('The property "addons" must be a list')
+	if len(machines) == 0:
+		raise UserError('Each service should have atleast one machine to serve traffic from.  Syntax: {"machines":["hostname-a"]}')
+	for addon in addons:
+		if addon not in globaladdons:
+			raise UserError('Can\'t find addon "'+addon+'"')
+
+	if servicetype == 'addon':
+		if 'name' not in data:
+			raise UserError('Addon has no name')
+		data['directorname'] = 'addon_'+ data['name']
+	else:
+		if 'hostname' not in data:
+			raise UserError('The property "hostname" is required.  This is normally dervied from a variable in the URL.')
+		
+		data['directorname'] = re.sub(r'\W+', '', data['hostname'])
+
+	# Put sanitised inputs back on the data dictionary
+	# (leave any other keys untouched)
+	data['machines'] = machines
+	data['addons'] = addons
+
+	# write the json to disk in case we need it later
+	with open('data/'+servicetype+'/'+data['directorname']+'.json', 'w') as outfile:
+		json.dump(data, outfile)
+
+	vcl = ""
+	backendnames = []
+	for backend in data['machines']:
+		if ':' in backend:
+			(backendhost, backendport) = backend.rsplit(':', 2)
+		else:
+			backendhost = backend
+			backendport = "80"
+		backendname = data['directorname'] + re.sub(r'\W+', '', backend)
+		backendnames.append(backendname)
+		vcl += "backend "+backendname+" {\n\t.host = \""+backendhost+"\";\n\t.port = \""+backendport+"\";\n\t.probe = {\n\t\t.url = \"/__gtg\";\n\t}\n}\n\n"
+	
+	vcl += "sub vcl_init {\n\tnew "+data['directorname']+" = directors.random();\n"
+	
+	for backendname in backendnames:
+		vcl += "\t"+data['directorname']+".add_backend("+backendname+", 1);\n"
+
+	vcl += "}\n\n"
+
+	if servicetype == 'service':
+		vcl += "sub "+servicetype+"_"+data['directorname']+" {\n\tif (req.http.Host == \""+data['hostname']+"\") {\n\t\tset req.backend_hint = "+data['directorname']+".backend();\n"
+
+		firstaddon = True
+		for addon in data['addons']:
+			if firstaddon:
+				vcl += "\t\tif "
+				firstaddon = False
+			else:
+				vcl += " elseif "
+			vcl += "(req.http.paas_addons !~ \""+addon+"\") {\n\t\t\tset req.backend_hint = addon_"+addon+".backend();\n\t\t}"
+
+		vcl += "\n\t}\n}\n\n"
+
+
+	# Write the VCL to disk
+	with open('/etc/varnish/'+servicetype+'/'+data['directorname']+'.vcl', 'w') as outfile:
+		outfile.write(vcl)
+
+globaladdons = {
+	'example': [
+		'ec2-52-16-13-243.eu-west-1.compute.amazonaws.com',
+	],
+}
+
+for name, machines in globaladdons.iteritems():
+	data = {
+		'name': name,
+		'machines': machines,
+	}
+	writeservice('addon', data)
+writedefaultvcl()
 
 #This class will handles any incoming request from
 #the browser 
@@ -50,10 +138,8 @@ class myHandler(BaseHTTPRequestHandler):
 			service = self.servicematch.search(self.path)
 			if service is not None:
 				hostname = service.group('hostname')
-
-				# write the json to disk in case we need it later
 				try:
-					with open('data/'+re.sub(r'\W+', '', hostname)+'.json', 'r') as outfile:
+					with open('data/service/'+re.sub(r'\W+', '', hostname)+'.json', 'r') as outfile:
 						output = outfile.read()
 				except IOError as e:
 					raise UserError("Service Not Found with hostname "+hostname)
@@ -83,7 +169,6 @@ class myHandler(BaseHTTPRequestHandler):
 		try:
 			service = self.servicematch.search(self.path)
 			if service is not None:
-				hostname = service.group('hostname')
 				if not 'Content-Length' in self.headers:
 					raise UserError("No Content-Length Header")
 				varLen = int(self.headers['Content-Length'])
@@ -93,55 +178,8 @@ class myHandler(BaseHTTPRequestHandler):
 				except:
 					raise UserError("Expected JSON")
 
-				# Validate inputs
-				machines = data.setdefault('machines', [])
-				addons = data.setdefault('addons', [])
-				if not isinstance(machines, list):
-					raise UserError('The property "machines" must be a list')
-				if not isinstance(addons, list):
-					raise UserError('The property "addons" must be a list')
-				if len(machines) == 0:
-					raise UserError('Each service should have atleast one machine to serve traffic from.  Syntax: {"machines":["hostname-a"]}')
-				
-				# Put sanitised inputs back on the data dictionary
-				# (leave any other keys untouched)
-				data['machines'] = machines
-				data['addons'] = addons
-				data['directorname'] = re.sub(r'\W+', '', hostname)
-
-				# write the json to disk in case we need it later
-				with open('data/'+data['directorname']+'.json', 'w') as outfile:
-					json.dump(data, outfile)
-
-				vcl = ""
-				backendnames = []
-				for backend in data['machines']:
-					if ':' in backend:
-						(backendhost, backendport) = backend.rsplit(':', 2)
-					else:
-						backendhost = backend
-						backendport = "80"
-					backendname = data['directorname'] + re.sub(r'\W+', '', backend)
-					backendnames.append(backendname)
-					vcl += "backend "+backendname+" {\n\t.host = \""+backendhost+"\";\n\t.port = \""+backendport+"\";\n\t.probe = {\n\t\t.url = \"/__gtg\";\n\t}\n}\n\n"
-				
-				vcl += "sub vcl_init {\n\tnew "+data['directorname']+" = directors.random();\n"
-				
-				for backendname in backendnames:
-					vcl += "\t"+data['directorname']+".add_backend("+backendname+", 1);\n"
-
-				vcl += "}\n\n"
-
-				vcl += "sub service_"+data['directorname']+" {\n\tif (req.http.Host == \""+hostname+"\") {\n\t\tset req.backend_hint = "+data['directorname']+".backend();\n\t}\n"
-
-				# TODO: addons go here
-
-				vcl += "}\n\n"
-
-
-				# Write the VCL to disk
-				with open('/etc/varnish/services/'+data['directorname']+'.vcl', 'w') as outfile:
-					outfile.write(vcl)
+				data['hostname'] = service.group('hostname')
+				writeservice('service', data)
 
 				writedefaultvcl()
 
