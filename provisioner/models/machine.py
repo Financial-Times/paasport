535e1c5836ef0d6d0c3f09cc28e759402a620db7
SamGiles <samuel.giles@ft.com>
2015-04-21 01:44:01 +0100

    Better formatting

--- provisioner/models/machine.py
+++ provisioner/models/machine.py
@@ -5,7 +5,8 @@ AMI_ID = 'ami-25158352'
 region = 'eu-west-1'
 
 def create_many(definitions, cluster_id):
-	return map(lambda data: create_new(data, cluster_id), definitions)
+	return map(format_instance, map(lambda data: create_new(data, cluster_id),
+		definitions))
 
 def create_new(data, cluster_id):
 	# Always keep the nursery full
@@ -15,7 +16,7 @@ def create_new(data, cluster_id):
 	instances = transfer_machine_from_nursery_to_cluster(cluster_id, new_name=name)
 
 	if len(instances) == 1:
-		return instances[0].id
+		return instances[0]
 
 	raise Exception("No available instances")
 
@@ -42,9 +43,22 @@ def transfer_machine_from_nursery_to_cluster(cluster_id, new_name="clustered_mac
 
 def get_instances_in_cluster(cluster_id):
 	connection = boto.ec2.connect_to_region(region)
-	return map(lambda instance: instance.id,
-			connection.get_only_instances(filters={ 'tag-key': 'cluster',
+	return 	map(format_instance, connection.get_only_instances(filters={ 'tag-key': 'cluster',
 				'tag-value': cluster_id }))
+
+def format_instance(instance):
+	return {
+		'id': instance.id,
+		'name': instance.tags['Name'],
+		'hostname': instance.private_ip_address,
+# hardcoded m3.medium
+		'cpu': 1,
+		'memory': '4026531840',
+		'disk': '4',
+		'region': region,
+		'metadata': '{}',
+		'state': instance.state
+	}
 
 def get_instance_info(instance_ids):
 	connection = boto.ec2.connect_to_region(region)
